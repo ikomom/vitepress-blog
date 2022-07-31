4da0e6a7856df881c276aba70bef6adad73be134
ikonon <315793076@qq.com>
2022-07-31 21:09:44 +0800

    update config

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,49 @@
-export default {
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
   title: 'VitePress',
-  description: 'Just playing around.'
-}
+  description: 'Just playing around.',
+  themeConfig: {
+    outlineTitle: 'In hac pagina',
+    sidebar: [
+      {
+        text: 'Guide',
+        items: [
+          { text: 'Introduction', link: '/' },
+          { text: 'Getting Started', link: '/getting-started' },
+        ]
+      }
+    ],
+    nav: [
+      {text: '算法', link: '/algorithm'},
+      {
+        text: 'Dropdown Menu',
+        items: [
+          { text: 'Item A', link: '/item-1' },
+          { text: 'Item B', link: '/item-2' },
+          { text: 'Item C', link: '/item-3' }
+        ]
+      }
+    ],
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+      { icon: 'twitter', link: '...' },
+      // You can also add custom icons by passing SVG as string:
+      {
+        icon: {
+          svg: '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Dribbble</title><path d="M12...6.38z"/></svg>'
+        },
+        link: '...'
+      }
+    ],
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2019-present Evan You'
+    },
+    docFooter: {
+      prev: 'Pagina prior',
+      next: 'Proxima pagina'
+    }
+  },
+
+})
--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,7 @@
+---
+title: Blogging Like a Hacker
+editLink: true
+
+---
+
 '# Hello VitePress' 
